358efab8d640c76a46c74ecd819e2d834d718eae
Tom Scharstein <trscharstein@gmail.com>
2021-02-02 11:37:24 -0700

    Initial version and documentation

---
+++ README.md
@@ -0,0 +1,212 @@
+# skullify
+
+[![NPM](https://nodei.co/npm/skullify.png)](https://npmjs.org/package/skullify)
+
+[See the demonstration panel here:](https://github.com/battleaxedotco/skullify-test-panel)
+
+![](https://thumbs.gfycat.com/ThirstyTenseCommabutterfly-small.gif)
+
+## Installation
+
+```bash
+npm install skullify
+```
+
+## Use
+
+```js
+// Import the skullify component
+import { skullify } from "skullify";
+
+export default {
+  // Register it as a component within the .vue file
+  components: {
+    skullify,
+  },
+};
+```
+
+```html
+<!--
+   Use it within this file's template:
+ -->
+<skullify
+  ref="icon"
+  folder="./src/assets/wrenches"
+  :options="{
+    autoplay: true,
+  }"
+/>
+<!-- 
+  Note that "ref" is required if you intend to shuffle animations
+ -->
+```
+
+# API
+
+`<skullify>`
+
+- Props
+- Methods
+- Events
+- Examples
+
+## `<skullify> props`
+
+| Prop              | Type     | Default                                                              |                                                                                                                                                                                                                 Description |
+| :---------------- | :------- | :------------------------------------------------------------------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| animationData?    | `Object` | `null`                                                               |                                                                                                                                             The contents of a Lottie file, takes precedence over `folder` and `files` props |
+| folder?           | `String` | `null`                                                               | The relative filepath (from panel root) to a folder containing lottie JSONs. Takes precedence over `files` but is overridden by `animationData`. All valid files within this folder will be in the "deck" as cards to draw. |
+| files?            | `Array`  | `[]`                                                                 |                                                                                               An array of relative filepaths from panel root to valid lottie JSON files. All valids will be in the "deck" as cards to draw. |
+| name?             | `String` | `(Generated UUID)`                                                   |                                                                                                             The unique identifier for this instance, in case advanced namespaced lottie methods are used within the project |
+| options?          | `Object` | `{ loop: false, prerender: true, autoplay: false, animType: "svg" }` |                                                                                                                                      Parameters for `lottie.loadAnimation` used on mount (or whenever the file is shuffled) |
+| uniqueRollLength? | `Number` | `1`                                                                  |                                     The length of roll history that any new roll for file or segment cannot match, e.g. `1` means new roll cannot be same as last roll, `2` means new roll cannot be same as last two rolls |
+| speed?            | `Number` | `1`                                                                  |                                                                                                                                                          The playback speed of the instance, with `1` being original AE fps |
+| direction?        | `Number` | `1`                                                                  |                                                                                                                                                               Whether animation is played forward (`1`) or in reverse(`-1`) |
+| skeleton?         | `Array`  | `[960, 540]`                                                         |                                                              Width and height dimensions used to create experimental CSS container aspect ratio matching animation to mask the initial paint of lottie (currently disabled) |
+
+---
+
+## `<skullify> methods`
+
+To allow multiple instances and control them individually, you must use standard Vue `ref`s:
+
+```html
+<skullify ref="target" />
+```
+
+```js
+export default {
+  mounted() {
+    const myComponent = this.$refs.target; // A reference pointer to the direct Vue data of this component
+    myComponent.shuffleFile(); // shuffleFile is a method of the child Vue component
+  },
+};
+```
+
+| Method               | Params           |                                                                                                                          Description |
+| :------------------- | :--------------- | -----------------------------------------------------------------------------------------------------------------------------------: |
+| `reconstruct()`      |                  | Destroys the current instance, removes all events, reinitializes according to the currently active index, and regenerates all events |
+| `shuffleFile()`      |                  |                                                   Rolls a random number to set as active index, then calls `reconstruct()` on itself |
+| `shuffleSegment()`   |                  |                                              Rolls a random number to set as active index, then calls `playSegmentChunk()` on itself |
+| `playSegmentChunk()` | index = `Number` |                                                               Plays the segment of frames between two comp marker locations by index |
+
+> Skullify includes all [native Lottie methods](https://github.com/airbnb/lottie-web#usage)
+
+---
+
+## `<skullify> events`
+
+| Event     | Params                  |                                                                                   Description |
+| :-------- | :---------------------- | --------------------------------------------------------------------------------------------: |
+| `@load`   |                         |             Called at the end of a `reconstruct()` method, when any new animation is rendered |
+| `@unload` |                         | Called at the beginning of a `reconstruct()` method, when any existing animation is destroyed |
+| `@error`  | errorMessage = `String` |                           Called during any method or parsing error with description of error |
+
+> Skullify includes all [native Lottie events](https://github.com/airbnb/lottie-web#events). All events are dash-case, e.g. "dom-loaded", "loop-complete", "loaded-images", etc.
+
+## Examples
+
+### Using the folder prop:
+
+```html
+<skullify
+  ref="icon"
+  folder="./src/assets/wrenches"
+  :options="{
+    autoplay: true,
+  }"
+/>
+```
+
+### Using the files prop:
+
+```html
+<skullify
+  ref="icon"
+  :files="[
+    './src/assets/wrenches/white.json',
+    './src/assets/wrenches/blue.json',
+    './src/assets/wrenches/green.json',
+    './src/assets/wrenches/yellow.json',
+  ]"
+  :options="{
+    autoplay: true,
+  }"
+/>
+```
+
+### Using the files prop with JSON data instead of file paths:
+
+```html
+<skullify
+  ref="icon"
+  :files="fileList"
+  :options="{
+    autoplay: true,
+  }"
+/>
+```
+
+```js
+export default {
+  data: () => ({
+    fileList: [
+      require("./assets/wrenches/blue.json"),
+      require("./assets/wrenches/green.json"),
+      require("./assets/wrenches/white.json"),
+      require("./assets/wrenches/yellow.json"),
+    ],
+  }),
+};
+```
+
+### Using the animationData prop:
+
+```html
+<skullify
+  ref="icon"
+  :animationData="currentAnimationData"
+  :options="{
+    autoplay: true,
+  }"
+/>
+```
+
+```js
+export default {
+  data: () => ({
+    activeIndex: 0, // Changing this value will cycle through the array
+    fileList: [
+      require("./assets/wrenches/blue.json"),
+      require("./assets/wrenches/green.json"),
+      require("./assets/wrenches/white.json"),
+      require("./assets/wrenches/yellow.json"),
+    ],
+  }),
+  computed: {
+    currentAnimationData(val) {
+      return this.fileList.length ? this.fileList[this.activeIndex] : null;
+    },
+  },
+};
+```
+
+### Using shuffleFile() in any of the above
+
+```js
+export default {
+  methods: {
+    randomizeAnimation() {
+      this.$refs.icon.shuffleFile();
+    },
+  },
+};
+```
+
+---
+
+## Notes and Todo
+
+- Skullify doesn't register it's content as static, and lazy loads. The first ~100ms result in it's container being minimally sized then expanding when the animation loads, pushing content below down and causing a noticeable unfolding effect. This is no issue when everything is centered or it's parent contains static `min-width` and `min-height`, but it should accept a `skeleton` to prevent this
+- shuffleSegment is not yet written, though it's been proven to work in the example panel
---
+++ components/skullify.vue
@@ -0,0 +1,367 @@
+<template>
+  <div class="skullify-container">
+    <slot v-if="$slots.default" />
+    <div
+      v-else
+      v-show="!mounted"
+      :style="buildSkeleton()"
+      class="skullify-skeleton"
+    />
+    <div class="skullify-animation" />
+  </div>
+</template>
+
+<script>
+import * as lottie from "lottie-web";
+import spy from "cep-spy";
+import path from "path";
+
+// Adobe is #1 concern
+const isAdobe = window.__adobe_cep__ ? true : false;
+
+// Node should be handled for filepaths via fs whenever not in Adobe
+const isNode =
+  typeof process !== "undefined" && process.release.name === "node";
+
+export default {
+  name: "skullify",
+  props: {
+    animationData: {
+      type: Object,
+      default: () => {
+        return null;
+      },
+    },
+    debug: {
+      type: Boolean,
+      default: false,
+    },
+    name: {
+      type: String,
+      default: () => {
+        return (
+          Date.now().toString(36) + Math.random().toString(36).substring(2)
+        );
+      },
+    },
+    options: {
+      type: Object,
+      default: () => {
+        return {
+          loop: false,
+          prerender: true,
+          autoplay: false,
+          animType: "svg",
+        };
+      },
+    },
+    folder: {
+      type: String,
+      default: "",
+    },
+    files: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    uniqueRollLength: {
+      type: Number,
+      default: 1,
+    },
+    speed: {
+      type: Number,
+      default: 1,
+    },
+    direction: {
+      type: Number,
+      default: 1,
+    },
+    skeleton: {
+      type: Array,
+      default: () => {
+        return [960, 960];
+      },
+    },
+  },
+  data: () => ({
+    animData: null,
+    opts: {
+      loop: false,
+      prerender: true,
+      autoplay: false,
+      animType: "svg",
+    },
+    realFiles: [],
+    activeFileIndex: 0,
+    activeSegmentIndex: 0,
+    previousFileRolls: [],
+    previousSegmentRolls: [],
+    loaded: false,
+    mounted: false,
+    evts: [
+      "complete",
+      "loopComplete",
+      "enterFrame",
+      "segmentStart",
+      "config_ready",
+      "data_ready",
+      "data_failed",
+      "loaded_images",
+      "DOMLoaded",
+      "destroy",
+    ],
+  }),
+  mixins: [require("../utils/IO").default, require("../utils/lottie").default],
+  async mounted() {
+    if (this.folder) {
+      if (this.debug) console.log("FOLDER:");
+      this.realFiles = await this.getFileListFromFolder();
+    } else if (this.files && this.files.length) {
+      if (this.debug) console.log("FILES:");
+      this.realFiles = await this.parseFileList();
+    }
+    await this.init();
+  },
+  watch: {
+    loaded(val) {
+      this.$emit(`${val ? "" : "un"}load`);
+    },
+    async animationData(val, last) {
+      if (JSON.stringify(val) !== JSON.stringify(last))
+        await this.reconstruct(val);
+    },
+    options: {
+      async handler(val, last) {
+        if (JSON.stringify(val) !== JSON.stringify(last))
+          await this.reconstruct(val);
+      },
+      deep: true,
+    },
+    speed(val, last) {
+      if (val !== last) this.setSpeed(val);
+    },
+    direction(val, last) {
+      if (val !== last) this.setDirection(val);
+    },
+    realFiles(val) {
+      if (this.debug) console.log("FILE LIST CHANGED:");
+      if (this.debug) console.log(val);
+    },
+  },
+  computed: {
+    markers() {
+      try {
+        return this.animData.animationData.markers;
+      } catch (err) {
+        return [];
+      }
+    },
+    segments() {
+      try {
+        if (!this.animData || !this.markers.length) return [];
+        let markers = this.markers.map((marker) => {
+          return marker.tm;
+        });
+        let list = [];
+        for (let i = 0; i < markers.length - 1; i++) {
+          const marker = markers[i];
+          const nextmarker = markers[i + 1];
+          let temp = {
+            forward: [marker, nextmarker],
+            reverse: [nextmarker, marker],
+          };
+          list.push(temp);
+        }
+        return list;
+      } catch (err) {
+        return [];
+      }
+    },
+    realDirection() {
+      return this.direction ? "forward" : "backward";
+    },
+    realFolderLocation() {
+      if (!this.folder || !this.folder.length) return null;
+      return this.sanitizeLocalPath(this.folder);
+    },
+  },
+  methods: {
+    convertToSnakeCase(str) {
+      return str !== "DOMLoaded"
+        ? (
+            str[0].toLowerCase() +
+            str
+              .slice(1, str.length)
+              .replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`)
+          ).replace("_", "-")
+        : "dom-loaded";
+    },
+    getGCR(a, b) {
+      return b == 0 ? a : this.getGCR(b, a % b);
+    },
+    getAspectRatio(a, b) {
+      let r = this.getGCR(a, b);
+      return `${a / r}:${b / r}`;
+    },
+    getRatio(str) {
+      console.log(str);
+      let string = str.split(":");
+      if (!string.length) return null;
+      return +string[0] / +string[1];
+    },
+    buildSkeleton() {
+      // if (this.mounted) {
+      console.log("SKELETON");
+      let ratio = this.getAspectRatio(960, 540);
+      let pratio = this.getRatio(ratio);
+      return `
+          overflow: hidden;
+          height: 0;
+          padding-top: ${(this.skeleton[0] / this.skeleton[1]) * 100}%;
+          width: 100%;
+          background: var(--color-bg);
+        `;
+      // }
+    },
+    sendEvent() {
+      if (!arguments.length || !arguments[0]) return null;
+      this.$emit(this.convertToSnakeCase(arguments[0].type), arguments[0]);
+    },
+    generateEvents() {
+      this.evts.forEach((evt) => {
+        this.animData.addEventListener(evt, this.sendEvent);
+      });
+    },
+    removeEvents() {
+      this.evts.forEach((evt) => {
+        this.animData.removeEventListener(evt, this.sendEvent);
+      });
+    },
+    async parseFileList() {
+      let temp = [];
+      for (let file of this.files.filter((file) => {
+        return /string/i.test(typeof file) ? /(json|lottie)$/.test(file) : true;
+      }))
+        if (/string/i.test(typeof file))
+          temp.push(
+            JSON.parse(await this.readFile(this.sanitizeLocalPath(file), false))
+          );
+        else if (/object/i.test(typeof file)) temp.push(file);
+        else this.$emit("error", `${typeof file} is not supported`);
+      return temp.filter((item) => {
+        return this.checkIfValidLottie(item);
+      });
+    },
+    checkIfValidLottie(item) {
+      return (
+        Object.keys(item).length &&
+        ["v", "fr", "ip"].some((el) => Object.keys(item).includes(el))
+      );
+    },
+    sanitizeLocalPath(targ) {
+      return /^\.\//.test(targ) && isAdobe
+        ? `${spy.path.root}/${targ.replace(/^\.\//, "")}`.replace(/\\/gm, "/")
+        : path.resolve(targ);
+    },
+    async getFileListFromFolder() {
+      if (this.debug) console.log("READING FROM FOLDER");
+      if (
+        this.folder &&
+        this.folder.length &&
+        this.realFolderLocation &&
+        this.realFolderLocation.length
+      ) {
+        let temp = await this.readFiles(this.realFolderLocation, false);
+        return temp
+          .filter((item) => {
+            return /string/i.test(typeof item);
+          })
+          .map((item) => {
+            return JSON.parse(item);
+          })
+          .filter((item) => {
+            return this.checkIfValidLottie(item);
+          });
+      } else if (this.files && this.files.length) {
+        // This should have been handled via "files" prop
+      } else return []; // This must be browser, we can't read files here
+    },
+    async reconstruct(val) {
+      this.removeEvents();
+      this.animData.destroy();
+      this.loaded = false;
+      if (val) await this.init();
+    },
+    async init() {
+      if (this.debug) {
+        console.log("INITIALIZING", this.animationData, this.animData);
+      }
+      try {
+        let temp = this.animationData
+          ? this.animationData
+          : this.realFiles.length
+          ? this.realFiles[this.activeFileIndex]
+          : null;
+
+        if (!temp) {
+          this.$emit("error", "Animation data was empty");
+          return null;
+        } else this.animData = this.buildAnimation(temp);
+        this.mounted = true;
+
+        if (this.opts.autoplay && this.animData && this.animData.play)
+          this.animData.play();
+        if (this.debug) console.log(this.animData);
+        this.loaded = true;
+        this.generateEvents();
+        return Promise.resolve(true);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    },
+    buildAnimation(anim = null) {
+      if (!anim) anim = this.animationData;
+      return lottie.loadAnimation(
+        Object.assign(
+          {
+            animationData: anim,
+            wrapper: this.$el.children[1],
+            name: this.name,
+          },
+          Object.assign(this.opts, this.options)
+        )
+      );
+    },
+    randomNum(min, max) {
+      return Math.floor(Math.random() * max) + min;
+    },
+    shuffleFile() {
+      while (this.previousFileRolls.length > this.uniqueRollLength)
+        this.previousFileRolls.shift();
+      let roll = this.rollRandom(this.realFiles.length, this.previousFileRolls);
+      this.previousFileRolls.push(roll);
+      this.activeFileIndex = roll;
+      this.reconstruct(true);
+    },
+    shuffleSegment() {},
+    rollRandom(max, excludes = []) {
+      let roll = this.randomNum(0, max);
+      return excludes.includes(roll) ? this.rollRandom(max, excludes) : roll;
+    },
+    playSegmentChunk(value) {
+      this.animData.playSegments(
+        this.segments[value][this.realDirection],
+        true
+      );
+      this.play();
+    },
+  },
+};
+</script>
+
+<style>
+.skullify-container svg {
+  width: 100%;
+}
+</style>
